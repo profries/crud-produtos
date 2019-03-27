3f0d6751f0e9dfae6424354f1833657a1b5465b1
Luis Ries <prof.ries@gmail.com>
2019-03-26 21:44:17 -0300

    Crud em memória finalizado!

--- main.php
+++ main.php
@@ -21,12 +21,42 @@
         return $GLOBALS['produtos'];        
     }
 
+    function deletar($id)
+    {
+        foreach($GLOBALS['produtos'] as $i => $produto) 
+        {
+            if($produto->id === $id)
+                array_splice($GLOBALS['produtos'],$i,1);    
+        }
+    }
+
+    function atualizar($id,Produto $produtoAlterado)
+    {
+        foreach($GLOBALS['produtos'] as $i => $produto) 
+        {
+            if($produto->id === $id)
+            {
+                $produto->nome = $produtoAlterado->nome;    
+                $produto->preco = $produtoAlterado->preco;
+            }
+        }
+        
+    }
 
 
     inserir(new Produto(1,"mesa",539.20));
     inserir(new Produto(2,"cadeira",135.30));
+    inserir(new Produto(3,"TV",3000));
+    inserir(new Produto(4,"radio",230));
 
     print_r(buscarPorId(1));
+
+    deletar(1);
+    
+    $produto = buscarPorId(4);
+    $produto->nome="dock";
+    atualizar(4,$produto);
+    
     print_r(listar());
 
 
